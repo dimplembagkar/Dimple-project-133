b2ed342f28a1fad5100b0b6823577982b6ec34bc
dimplembagkar <84089522+dimplembagkar@users.noreply.github.com>
2022-08-11 16:07:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function Dog()
+{
+    window.location="index1.html";
+}
+
+function Scale()
+{
+    window.location="index2.html";
+}
+
+function remote()
+{
+    window.location="index3.html";
+}
